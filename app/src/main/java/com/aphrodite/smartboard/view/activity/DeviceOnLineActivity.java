f0cc09033a69580be12e7c8c841c23e36cdea9ef
Aphrodite123 <927467936@qq.com>
2020-08-18 21:36:34 +0800

    在线数据生成视频

--- app/src/main/java/com/aphrodite/smartboard/view/activity/DeviceOnLineActivity.java
+++ app/src/main/java/com/aphrodite/smartboard/view/activity/DeviceOnLineActivity.java
@@ -1,14 +1,16 @@
 package com.aphrodite.smartboard.view.activity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
-
-import androidx.lifecycle.Observer;
 
 import com.apeman.sdk.bean.BoardType;
 import com.apeman.sdk.bean.DevicePoint;
@@ -18,13 +20,23 @@ import com.apeman.sdk.service.PenService;
 import com.apeman.sdk.service.command.Command;
 import com.apeman.sdk.widget.BoardView;
 import com.apeman.sdk.widget.BoardViewCallback;
+import com.aphrodite.framework.utils.ObjectUtils;
 import com.aphrodite.framework.utils.ToastUtils;
 import com.aphrodite.framework.utils.UIUtils;
 import com.aphrodite.smartboard.R;
 import com.aphrodite.smartboard.config.AppConfig;
+import com.aphrodite.smartboard.model.bean.CW;
+import com.aphrodite.smartboard.model.bean.CWACT;
+import com.aphrodite.smartboard.model.bean.CWLine;
 import com.aphrodite.smartboard.model.bean.ScreenRecordEntity;
+import com.aphrodite.smartboard.model.bean.ShareContentType;
+import com.aphrodite.smartboard.model.ffmpeg.FFmpegHandler;
 import com.aphrodite.smartboard.utils.BitmapUtils;
 import com.aphrodite.smartboard.utils.CWFileUtils;
+import com.aphrodite.smartboard.utils.FFmpegUtils;
+import com.aphrodite.smartboard.utils.FileUtil;
+import com.aphrodite.smartboard.utils.FileUtils;
+import com.aphrodite.smartboard.utils.Share2;
 import com.aphrodite.smartboard.view.activity.base.BaseDeviceActivity;
 import com.aphrodite.smartboard.view.widget.dialog.DeleteDialog;
 
@@ -33,11 +45,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.lifecycle.Observer;
 import butterknife.BindView;
 import butterknife.OnClick;
 import kotlin.Unit;
 import kotlin.jvm.functions.Function0;
 import kotlin.jvm.functions.Function1;
+
+import static com.aphrodite.smartboard.model.ffmpeg.FFmpegHandler.MSG_BEGIN;
+import static com.aphrodite.smartboard.model.ffmpeg.FFmpegHandler.MSG_FINISH;
 
 /**
  * Created by Aphrodite on 20-6-19
@@ -66,6 +82,14 @@ public class DeviceOnLineActivity extends BaseDeviceActivity {
     private List<DevicePoint> mDevicePoints;
     private List<ScreenRecordEntity> data = new ArrayList<>();
 
+    private static int LINE_PARTS = 5;
+    private CW mCw;
+    private List<List<Integer>> mPoints;
+    private float mLastX;
+    private float mLastY;
+    private int mImageNum;
+    private FFmpegHandler mFfmpegHandler;
+
     @Override
     protected int getViewId() {
         return R.layout.activity_device_online;
@@ -88,6 +112,8 @@ public class DeviceOnLineActivity extends BaseDeviceActivity {
 
     @Override
     protected void initData() {
+        mFfmpegHandler = new FFmpegHandler(mHandler);
+        mPoints = new ArrayList<>();
     }
 
     @Override
@@ -235,7 +261,134 @@ public class DeviceOnLineActivity extends BaseDeviceActivity {
         data.add(screenRecordEntity);
 
         mTimestamp = System.currentTimeMillis();
-        CWFileUtils.write(data, AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA + mTimestamp + File.separator, mCanvasWidth, mCanvasHeight, mTimestamp);
+        CWFileUtils.write(data, AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA, mCanvasWidth, mCanvasHeight, mTimestamp);
+    }
+
+    private void pathsCut() {
+        mCw = CWFileUtils.read(AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA + AppConfig.DATA_FILE_NAME);
+        if (null == mCw) {
+            return;
+        }
+
+        List<CWACT> cwacts = mCw.getACT();
+        if (null == cwacts || cwacts.size() <= 0) {
+            return;
+        }
+
+        for (CWACT cwact : cwacts) {
+            if (null == cwact) {
+                continue;
+            }
+
+            splitLine(cwact.getLine());
+        }
+        handlePhoto();
+    }
+
+    private void splitLine(CWLine cwLine) {
+        if (null == cwLine) {
+            return;
+        }
+
+        String[] split = cwLine.getColor().split("\\,");
+        int color = Color.rgb(Integer.valueOf(split[0]), Integer.valueOf(split[1]), Integer.valueOf(split[2]));
+        int width = cwLine.getWidth();
+
+        List<List<Integer>> points = cwLine.getPoints();
+        if (null == points || points.size() <= 0) {
+            return;
+        }
+
+        //将线条进行分割
+        int consult = points.size() / LINE_PARTS;
+        if (consult > 1) {
+            List<List<Integer>> splitPoints = new ArrayList<>();
+            for (int i = 1; i < consult + 1; i++) {
+                splitPoints.clear();
+
+                if (1 == i) {
+                    splitPoints.addAll(points.subList(0, LINE_PARTS));
+                } else if (consult == i) {
+                    splitPoints.addAll(points.subList(LINE_PARTS * (i - 1) - 2, points.size()));
+                } else {
+                    splitPoints.addAll(points.subList(LINE_PARTS * (i - 1) - 2, LINE_PARTS * i));
+                }
+                drawPathToBitmap(color, width, splitPoints);
+            }
+        } else {
+            drawPathToBitmap(color, width, points);
+        }
+    }
+
+    private void drawPathToBitmap(int color, int width, List<List<Integer>> points) {
+        if (null == points || points.size() <= 0) {
+            return;
+        }
+
+        mPoints.addAll(points);
+
+        Bitmap bitmap = Bitmap.createBitmap(mCanvasWidth, mCanvasHeight, Bitmap.Config.ARGB_8888);
+        bitmap.setHasAlpha(true);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.WHITE);
+        initPaint();
+
+        List<Integer> xyPoints;
+        for (int i = 0; i < mPoints.size(); i++) {
+            xyPoints = mPoints.get(i);
+            if (ObjectUtils.isOutOfBounds(xyPoints, 2) || xyPoints.get(2) <= 0) {
+                continue;
+            }
+            if (0 == i) {
+                canvas.drawPoint((float) (xyPoints.get(0) * mXScale), (float) (xyPoints.get(1) * mYScale), mPaint);
+            } else {
+                drawPath(canvas, mLastX, mLastY, (float) (xyPoints.get(0) * mXScale), (float) (xyPoints.get(1) * mYScale));
+            }
+            mLastX = (float) (xyPoints.get(0) * mXScale);
+            mLastY = (float) (xyPoints.get(1) * mYScale);
+        }
+
+        StringBuffer imageName = new StringBuffer();
+        imageName.append("img").append(mImageNum).append(".jpg");
+        try {
+            BitmapUtils.saveBitmap(bitmap, AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA, imageName.toString(), Bitmap.CompressFormat.JPEG, 100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mImageNum++;
+    }
+
+    private void drawPath(Canvas canvas, float startX, float startY, float endX, float endY) {
+        float x = endX - startX;
+        float y = endY - startY;
+        //10倍插点
+        int insertCount = (int) (Math.max(Math.abs(x), Math.abs(y)) + 2);
+        //S.i("补点：$insertCount")
+        float dx = x / insertCount;
+        float dy = y / insertCount;
+        for (int i = 0; i < insertCount; i++) {
+            float insertX = startX + i * dx;
+            float insertY = startY + i * dy;
+            canvas.drawPoint(insertX, insertY, mPaint);
+        }
+    }
+
+    /**
+     * 图片合成视频
+     */
+    private void handlePhoto() {
+        // 图片所在路径，图片命名格式img+number.jpg
+        // 这里指定目录为根目录下img文件夹
+        if (!FileUtils.isExist(AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA)) {
+            return;
+        }
+
+        String combineVideo = AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA + "video.mp4";
+        int frameRate = 10;// 合成视频帧率建议:1-10  普通视频帧率一般为25
+        String[] commandLine = FFmpegUtils.pictureToVideo(AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA, frameRate, combineVideo);
+        if (mFfmpegHandler != null) {
+            mFfmpegHandler.executeFFmpegCmd(commandLine);
+        }
     }
 
     private void cleanBoardView() {
@@ -298,6 +451,7 @@ public class DeviceOnLineActivity extends BaseDeviceActivity {
     @OnClick(R.id.iv_right_btn)
     public void onToolbarRightBtn() {
         saveData();
+        pathsCut();
     }
 
     private DeleteDialog.OnClickListener mClickListener = new DeleteDialog.OnClickListener() {
@@ -309,6 +463,40 @@ public class DeviceOnLineActivity extends BaseDeviceActivity {
         @Override
         public void onPositive() {
             savePicture();
+        }
+    };
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_BEGIN:
+                    showLoadingDialog();
+                    break;
+                case MSG_FINISH:
+                    dismissLoadingDialog();
+                    if (null != mPoints) {
+                        mPoints.clear();
+                    }
+
+                    String combineVideo = AppConfig.DATA_PATH + AppConfig.PATH_ONLINE_DATA + "video.mp4";
+                    Uri shareFileUrl = FileUtil.getFileUri(DeviceOnLineActivity.this, ShareContentType.VIDEO, new File(combineVideo));
+                    if (null == shareFileUrl) {
+                        return;
+                    }
+                    new Share2.Builder(DeviceOnLineActivity.this)
+                            .setContentType(ShareContentType.VIDEO)
+                            .setShareFileUri(shareFileUrl)
+                            .setTitle(getString(R.string.app_name))
+                            .build()
+                            .shareBySystem();
+
+                    break;
+                default:
+                    break;
+            }
         }
     };
 
